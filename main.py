206a23a8ea5922c7b86da4fb98123f1255101b0d
ISHIMWE DEO <email_yawe@gmail.com>
2026-02-21 17:35:44 +0200

    Update dependencies in requirements

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from libretranslate import main
+import os
+
+from libretranslate.app import create_app
+from libretranslate.main import get_args
+
+args = get_args()
+app = create_app(args)
 
 if __name__ == "__main__":
-    main()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
