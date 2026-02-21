cc301676ee1fa8d3a8f06a4b61ea950f30789c99
ISHIMWE DEO <email_yawe@gmail.com>
2026-02-21 23:15:51 +0200

    Remove install from start script

--- start.sh
+++ start.sh
@@ -4,7 +4,4 @@ set -euo pipefail
 # Render sets PORT. Default to 10000 for local use.
 PORT="${PORT:-10000}"
 
-# Install the package in editable mode
-pip install -e .
-
 exec libretranslate --host 0.0.0.0 --port $PORT
