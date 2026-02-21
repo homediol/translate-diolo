bc83236252f626ad84442eef221f21e80997730e
ISHIMWE DEO <email_yawe@gmail.com>
2026-02-21 23:19:59 +0200

    Use python -m to avoid directory name conflict

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 # Render sets PORT. Default to 10000 for local use.
 PORT="${PORT:-10000}"
 
-exec libretranslate --host 0.0.0.0 --port $PORT
+exec python -m libretranslate.main --host 0.0.0.0 --port $PORT
